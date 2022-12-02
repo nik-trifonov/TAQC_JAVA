a1b652d6668ad53883c5e42ffb6bee3a1881b2e8
nik-trifonov <102224540+nik-trifonov@users.noreply.github.com>
2022-12-02 14:44:22 +0000

    Add files via upload

---
+++ Task.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class Task {
+    /*public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome");
+        System.out.println("Please");
+        int height = scanner.nextInt();
+        if(height < 130) {
+            System.out.println("YOu are too short");
+        } else {
+            System.out.println("Wow");
+        }*/
+    /*public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("The vip Lounge");
+        System.out.println("Age");
+        int age = scanner.nextInt();
+        System.out.print("Do you have a Vip/pass? yes/no");
+        String vip = scanner.next();
+        if(age >= 18 && vip.equals("yes")){
+            System.out.println("Go on");
+        } else {
+            System.out.println("Thanks");
+        }*/
+    public static void main(String[] args) {
+        int i =0;
+        do {
+            i++;
+        } while (i < 5);
+    }
+}
+
+
+
+
+
+
